4a2abd3783922acb9b6d539a9c43366c64a5bd4b
Dragary <95122611+Dragary@users.noreply.github.com>
2022-03-07 17:12:17 +0500

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  access_key = "         "
+  secret_key = "         "
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "pUbuntu" {
+  ami                    = "ami-04505e74c0741db8d"
+  instance_type          = "t3.micro"
+  key_name               = "key"
+  vpc_security_group_ids = [aws_security_group.webserv.id]
+  user_data              = file("s-data.sh")
+}
+
+
+resource "aws_security_group" "webserv" {
+  name        = "WebServer-SG"
+  description = "MySG"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
